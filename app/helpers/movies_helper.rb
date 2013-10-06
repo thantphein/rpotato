18d625a1055b39beabcaf5e42297b73ad9a13c04
saasbook <saasbook@saasbook.(none)>
2013-10-06 08:07:56 -0700

    final submission

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -4,21 +4,6 @@ module MoviesHelper
     count.odd? ?  "odd" :  "even"
   end
 
-  def helper_class(field)
-    if (params[:sort_by].to_s == field)
-      return 'hilite'
-    end
-    return nil
-  end
-
-  def helper_sort(movie)
-    if (params[:sort_by].to_s == 'title')
-      return movie.title
-    elsif (params[:sort_by].to_s == 'release_date')
-      return movie.release_date.to_s
-    end
-  end
-
   def helper_select(movie)
     if (params[:ratings] == nil)
       return true
